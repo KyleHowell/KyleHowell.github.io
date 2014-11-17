a59ed8a8e9ce00cdf5c4f2a22331f3c40bfa7f22
Kyle Howell <mrkylehowell@gmail.com>
2014-11-17 13:22:41 -0800

    add d3 and leaflet blogs

--- js/d3.js
+++ js/d3.js
@@ -1,32 +1,32 @@
 d3.select("body").append("h2").text("Upfront Monthly Cost");
 
-serverType = ["t2.mirco", "t2.small", "t2.medium", "m3.medium", "m3.large", "m3.xlarge", "m3.2xlarge"];
-upfrontCost = [68, 136, 272, 222, 443, 886, 1772];
-hourlyCost = [0.004, 0.008, 0.016, 0.029, 0.057, 0.113, 0.228];
+serverType = ["t2.mirco", "t2.small", "t2.medium", "m3.medium", "m3.large", "m3.xlarge"];
+upfrontCost = [68, 136, 272, 222, 443, 886];
+hourlyCost = [0.004, 0.008, 0.016, 0.029, 0.057, 0.113];
 
 var svgWidth = 900;
 var svgHeight = 200;
 var barPadding = 5;
-var svg = d3.select("body").append("svg").attr("width", svgWidth).attr("height", svgHeight)
+var svgUpfront = d3.select("body").append("svg").attr("width", svgWidth).attr("height", svgHeight)
   .style("display", "block").style("margin", "auto");
 
-svg.selectAll("rect").data(upfrontCost).enter()
+svgUpfront.selectAll("rect").data(upfrontCost).enter()
   .append("rect")
   .attr("x", function(d,i){
     return i * (svgWidth / upfrontCost.length);
   })
   .attr("y", function(d){
-    return svgHeight - (d/10);
+    return svgHeight - (d/5);
   })
   .attr("width", svgWidth / upfrontCost.length - barPadding)
   .attr("height", function(d){
-    return d / 10;
+    return d / 5;
   })
   .attr("fill", function(d){
-    return "rgb(" + d + ",0,0)";
+    return "rgb(" + Math.floor(d /2)  + ",0,0)";
   });
 
-svg.selectAll("text").data(upfrontCost).enter()
+svgUpfront.selectAll("text").data(upfrontCost).enter()
    .append("text")
    .text(function(d, i){
     return serverType[i] + " " + d + "$";
@@ -41,4 +41,38 @@ svg.selectAll("text").data(upfrontCost).enter()
   .attr("font-size", "15px")
   .attr("fill", "purple");
 
-d3.select("body").append("h2").text("Hourly Cost");
+d3.select("body").append("h2").text("Hourly Cost per Month");
+
+var svgHourly = d3.select("body").append("svg").attr("width", svgWidth).attr("height", svgHeight)
+  .style("display", "block").style("margin", "auto");
+
+svgHourly.selectAll("rect").data(hourlyCost).enter()
+  .append("rect")
+  .attr("x", function(d,i){
+    return i * (svgWidth / hourlyCost.length);
+  })
+  .attr("y", function(d){
+    return svgHeight - (d*1500);
+  })
+  .attr("width", svgWidth / upfrontCost.length - barPadding)
+  .attr("height", function(d){
+    return d * 1500;
+  })
+  .attr("fill", function(d){
+    return "rgb(" + Math.floor(d * 6000) + ",0,0)";
+  });
+
+svgHourly.selectAll("text").data(hourlyCost).enter()
+   .append("text")
+   .text(function(d, i){
+    return serverType[i] + " $" + Math.floor(d*730.484);
+   })
+   .attr("x", function(d,i){
+    return i * (svgWidth / hourlyCost.length);
+  })
+  .attr("y", function(d){
+    return svgHeight - 180;
+  })
+  .attr("font-family", "sans-serif")
+  .attr("font-size", "15px")
+  .attr("fill", "purple");
--- js/leaflet.js
+++ js/leaflet.js
@@ -1,20 +1,65 @@
+//Map with on click
 var map = L.map('map').setView([37.77484477446375, -122.43481636047363], 13);
-
 L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
-  maxZoom: 18,
-  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  maxZoom: 15,
+  minZoom:12,
   id: 'examples.map-i875mjb7'
 }).addTo(map);
 
 var popup = L.popup();
-
 function onMapClick(e) {
   popup
     .setLatLng(e.latlng)
     .setContent("You clicked the map at " + e.latlng.toString())
     .openOn(map);
 }
-
 map.on('click', onMapClick);
+
+//Zoomed map with markers
+var mapZoom = L.map('mapZoom').setView([37.7797, -122.41379], 14);
+L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+  minZoom: 13,
+  id: 'examples.map-i875mjb7'
+}).addTo(mapZoom);
+
+var locations = [["City Center Farmers Market 9-11AM", 37.77967, -122.41366],
+  ["Pine and Battery 11AM-1PM", 37.79157, -122.39967],
+  ["Soma StrEat Food Park 2PM-7PM", 37.76943, -122.40928]
+  ];
+
+for (var i = 0; i < locations.length; i++) {
+  marker = new L.marker([locations[i][1],locations[i][2]])
+    .bindPopup(locations[i][0])
+    .addTo(mapZoom);
+    }
+
+//Map with different layers
+var mapLayers = L.map('mapLayers').setView([37.77484477446375, -122.43481636047363], 13);
+L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+  maxZoom: 15,
+  minZoom:12,
+  id: 'examples.map-i875mjb7'
+}).addTo(mapLayers);
+
+var truckOne = new L.LayerGroup();
+L.marker([37.79157, -122.39967])
+    .bindPopup('Truck One 9-11AM').addTo(truckOne),
+L.marker([37.77967, -122.41366])
+    .bindPopup('Truck One 11AM-1PM').addTo(truckOne),
+L.marker([37.76943, -122.40928])
+    .bindPopup('TruckOne 1PM-7PM').addTo(truckOne),
+L.polyline([[37.79157, -122.39967],[37.77967, -122.41366],[37.76943, -122.40928]]).addTo(truckOne);
+
+var truckTwo = new L.LayerGroup();
+L.marker([37.78913, -122.39675])
+    .bindPopup('Truck Two 9-11AM').addTo(truckTwo),
+L.marker([37.78866, -122.40641])
+    .bindPopup('Truck Two 11AM-1PM').addTo(truckTwo),
+L.marker([37.78181, -122.4007])
+    .bindPopup('Truck Two 1PM-7PM').addTo(truckTwo),
+L.polyline([[37.78913, -122.39675],[37.78866, -122.40641],[37.78181, -122.4007]]).addTo(truckTwo);
+
+var overlays = {"Food Truck One": truckOne, "Food Truck Two": truckTwo};
+L.control.layers(overlays).addTo(mapLayers);
+
